9fbb6d175cd90146d1b7330b2e54fc75f08ea2c2
JuanZurita96 <81644288+JuanZurita96@users.noreply.github.com>
2021-03-30 11:45:50 -0600

    Add files via upload

---
+++ MVC/app/vistas/paginas/inicio.php
@@ -0,0 +1,4 @@
+<!-- Pagina de Inicio (Landing Page) -->
+<?php require RUTA_APP . '/vistas/inc/header.php';?>
+<h2><?php echo $datos['titulo'] ?></h2>
+<?php require RUTA_APP . '/vistas/inc/footer.php';?>
