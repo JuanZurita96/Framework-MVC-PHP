0ba40cb5ab97c9df85c0bb769c60a3ea85dc8aec
JuanZurita96 <81644288+JuanZurita96@users.noreply.github.com>
2021-03-30 11:50:04 -0600

    Delete app directory

--- app/vistas/inc/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Archivo Footer -->
-<script type="text/javascript" src="<?php echo RUTA_URL;?>/js/main.js"></script> 
-</body>
-</html>
--- app/vistas/paginas/inicio.php
+++
@@ -1,4 +0,0 @@
-<!-- Pagina de Inicio (Landing Page) -->
-<?php require RUTA_APP . '/vistas/inc/header.php';?>
-<h2><?php echo $datos['titulo'] ?></h2>
-<?php require RUTA_APP . '/vistas/inc/footer.php';?>
